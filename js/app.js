71712d7811898f058d1708b773d8a603ec20cde2
Richard Joffray <richardj@maxsampartners.com>
2015-08-29 20:29:08 -0700

    orientation fix

--- js/app.js
+++ js/app.js
@@ -87,13 +87,13 @@ _.mixin({
     }
 });
 
-$(document).ready(function () {
-    function reorient(e) {
-        var portrait = (window.orientation % 180 == 0);
-        $("body > div").css("-webkit-transform-origin", "0 0");
-        $("body > div").css("-webkit-transform", !portrait ? "rotate(0deg)" : "");
-    }
-    window.onorientationchange = reorient;
-    window.setTimeout(reorient, 0);
-});
+//$(document).ready(function () {
+//    function reorient(e) {
+//        var portrait = (window.orientation % 180 == 0);
+//        $("body > div").css("-webkit-transform-origin", "0 0");
+//        $("body > div").css("-webkit-transform", !portrait ? "rotate(0deg)" : "");
+//    }
+//    window.onorientationchange = reorient;
+//    window.setTimeout(reorient, 0);
+//});
 
