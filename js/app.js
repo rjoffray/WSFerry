d469e53e52d57f83b49dd54ebb801a98d6c3408e
Richard Joffray <richardj@maxsampartners.com>
2015-08-29 20:23:05 -0700

    orientation fix

--- js/app.js
+++ js/app.js
@@ -90,6 +90,7 @@ _.mixin({
 $(document).ready(function () {
     function reorient(e) {
         var portrait = (window.orientation % 180 == 0);
+        $("body > div").css("-webkit-transform-origin", "0 0");
         $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
     }
     window.onorientationchange = reorient;
